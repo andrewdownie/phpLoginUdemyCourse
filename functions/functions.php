5fd827247d322ec4c8bc87baa6e8500d37996322
andrew downie <andrew_dev@outlook.com>
2016-05-13 14:55:42 -0400

    Ready for tut10. Setup the register form, and grabbing the variables server side.

--- functions/functions.php
+++ functions/functions.php
@@ -31,4 +31,17 @@ function token_generator(){
 //====== VALIDATION FUNCTIONS --------------------------------------------------
 //======
 
+
+function validate_user_registration(){
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+
+        $first_name        = clean($_POST['first_name']);
+        $last_name         = clean($_POST['last_name']);
+        $username          = clean($_POST['username']);
+        $email             = clean($_POST['email']);
+        $password          = clean($_POST['password']);
+        $confirm_password  = clean($_POST['confirm_password']);
+    }
+}
+
 ?>
--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 
 	<div class="row">
 		<div class="col-lg-6 col-lg-offset-3">
-
+			<?php validate_user_registration() ?>
 
 		</div>
 
@@ -28,7 +28,12 @@
 						<div class="row">
 							<div class="col-lg-12">
 								<form id="register-form" method="post" role="form" >
-
+									<div class="form-group">
+										<input type="text" name="first_name" id="first_name" tabindex="1" class="form-control" placeholder="First Name" value="" required >
+									</div>
+									<div class="form-group">
+										<input type="text" name="last_name" id="last_name" tabindex="1" class="form-control" placeholder="Last Name" value="" required >
+									</div>
 									<div class="form-group">
 										<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="" required >
 									</div>
