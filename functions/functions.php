af4553837b8201025fd2d27987ad3b122c6a025b
andrew downie <andrew_dev@outlook.com>
2016-05-13 15:58:17 -0400

    Ready for tut 12. Made the errors pretty.

--- functions/functions.php
+++ functions/functions.php
@@ -54,7 +54,15 @@ function validate_user_registration(){
 
         if(!empty($errors)){
             foreach($errors as $error){
-                echo $error . "<br>";
+
+                echo "
+
+                <div class='alert alert-danger alert-dismissible' role='alert'>
+                    <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+                    <strong>Warning!</strong>
+                    {$error}
+                </div>";
+
             }
         }
     }
